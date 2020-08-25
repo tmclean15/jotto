303ee7423878156a6138a817099381e11a1a34f3
Taylor McLean <taylor@taylormclean.software>
2020-08-25 10:07:31 -0400

    Initial redux setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import './App.css'
 
 import { GuessedWords, Congrats } from './components'
 
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {*} action - Action to be reduced.
+ * @returns {boolean} - New success state.
+ */
+export default function successReducer(state, action) {
+  return null
+}
