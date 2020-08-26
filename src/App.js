61e5f6628b44c25824b5c0b4515a13f3524d1969
Taylor McLean <taylor@taylormclean.software>
2020-08-26 14:55:55 -0400

    Removed redundant imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components'
 
 import { GuessedWords, Congrats, Input } from './components'
